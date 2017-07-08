e08c01481af1f7a16c432b793e335950f4fdb4f9
Hoang Nguyen <nphoang1102@gmail.com>
2017-07-08 13:26:53 -0700

    changed the sublime preferene to replace tab to space. Also the credential helper seems to be working so far

--- project0/ps0.py
+++ project0/ps0.py
@@ -2,17 +2,17 @@
 from numpy import log2
 
 def main():
-	# Input is read in as a strin, thus need casting
-	x = int(input("Enter number x: "))
-	y = int(input("Enter number y: "))
+   # Input is read in as a strin, thus need casting
+   x = int(input("Enter number x: "))
+   y = int(input("Enter number y: "))
 
-	# Comput computation here and printout to screen
-	print("x**y =", (x**y))
-	print("log(x) =", (log2(x)))
-	# Does not require casting if using comma, pretty neat
+   # Comput computation here and printout to screen
+   print("x**y =", (x**y))
+   print("log(x) =", (log2(x)))
+   # Does not require casting if using comma, pretty neat
 
-	# Done
-	return
+   # Done
+   return
 
 # Invoke main
 main()
