d98ae141c644c7b9e9dd1257e69bbb20cd66e9d5
Hoang Nguyen <nphoang1102@gmail.com>
2017-07-30 17:57:05 -0700

    aha finally my first recursion program! Also I finally found enough determination to resume taking this course

--- project4/ps4a.py
+++ project4/ps4a.py
@@ -1,9 +1,20 @@
 # Problem Set 4A
-# Name: <your name here>
+# Name: Hoang Nguyen
 # Collaborators:
-# Time Spent: x:xx
+# Time Spent: 1:00
 
 def get_permutations(sequence):
+    permu = list()
+    if len(sequence) <= 1:
+        permu.append(sequence)
+    else:
+        body = sequence[1:]
+        list_body = get_permutations(body)
+        for char in list_body:
+            for i in range(len(char) + 1):
+                permu.append(char[0:i] + sequence[0] + char[i:])
+    return permu
+
     '''
     Enumerate all permutations of a given string
 
@@ -26,6 +37,7 @@ def get_permutations(sequence):
     pass #delete this line and replace with your code here
 
 if __name__ == '__main__':
+    print(get_permutations('abc'))
 #    #EXAMPLE
 #    example_input = 'abc'
 #    print('Input:', example_input)
@@ -36,5 +48,5 @@ if __name__ == '__main__':
 #    to be three characters or fewer as you will have n! permutations for a 
 #    sequence of length n)
 
-    pass #delete this line and replace with your code here
+    # pass #delete this line and replace with your code here
 
